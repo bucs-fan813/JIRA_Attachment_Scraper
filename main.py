0832abe758aef251ecb559f580bdb020e5e6396d
John Russell <jrussell@casteliasystems.com>
2022-04-23 15:16:33 -0400

    added user scrapping

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ from os import makedirs
 from os.path import join, dirname, realpath, exists
 from dotenv import dotenv_values
 from requests.auth import HTTPBasicAuth
+from progress.bar import Bar
+
 
 PROJECT_DIRECTORY = dirname(realpath(__file__))
 ATTACHMENTS_DIRECTORY = join(PROJECT_DIRECTORY, "attachments")
-JSON_FILE = join(PROJECT_DIRECTORY, "response.json2")
+JSON_FILE = join(PROJECT_DIRECTORY, "attachmentsq.json")
 ENV_FILE = join(PROJECT_DIRECTORY, ".env")
 USERNAME = ''
 PASSWORD = ''
@@ -38,8 +40,10 @@ if __name__ == "__main__":
     with open(JSON_FILE, 'r') as content:
         print("Reading JSON data")
         data = json.load(content)
+        bar = Bar('Downloading', max=len(data["issues"]), suffix='%(percent)d%% [%(index)s / %(max)s]')
 
         for issue in data["issues"]:
+            bar.next()
             if issue["fields"]["attachment"]:
                 for attachment in issue["fields"]["attachment"]:
                     item = {
@@ -65,9 +69,11 @@ if __name__ == "__main__":
                     try:
                         r = requests.get(item['url'], auth=HTTPBasicAuth(USERNAME, PASSWORD), allow_redirects=True)
                         if r.status_code == HTTPStatus.OK:
-                            print(f"Downloading {r.url} to -> {ATTACHMENT_FILE} [{r.status_code}]")
+                            # print(f"Downloading {r.url} to -> {ATTACHMENT_FILE} [{r.status_code}]")
                             open(ATTACHMENT_FILE, 'wb+').write(r.content)
                     except urllib3.exceptions.HTTPError as e:
                         print(e)
                     attachments += [item]
+            bar.finish()
+
         print(f"Found: {len(attachments)} attachments")
